ab27b49d57e6a52ebc34eb4c4e2f0a3514f5af25
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-04-29 15:01:58 -0300

    base http adapter

--- packages/api/src/adapters/base.js
+++ packages/api/src/adapters/base.js
@@ -1,5 +1,25 @@
 class BaseAdapter {
+  $makePayload (...args) {
+    if (args.length === 1) {
+      const payload = args[0]
+      return payload.id ? payload : { ...payload, id: Math.random() }
+    }
 
+    const id = Math.random()
+
+    if (args.length === 2) {
+      const [ method, params ] = args
+      return { method, params, id }
+    }
+
+    const [ api, method, params ] = args
+
+    return {
+      id,
+      params,
+      method: `${api}.${method}`
+    }
+  }
 }
 
 export default BaseAdapter
--- packages/api/src/adapters/http.js
+++ packages/api/src/adapters/http.js
@@ -1,10 +1,69 @@
 import BaseAdapter from './base'
 
-class HttpAdapter extends BaseAdapter {
-  constructor (fetchFn) {
-    super()
+class RPCError extends Error {
+  constructor (rpcError) {
+    super(rpcError.message)
+    this.name = 'RPCError'
+    this.code = rpcError.code
+    this.data = rpcError.data
+  }
+}
 
-    this.$fetch = fetchFn
+class HttpAdapter extends BaseAdapter {
+  constructor (options = {}) {
+    super()
+    this.$parseOptions(options)
+  }
+  $parseOptions (options) {
+    const { fetch, uri } = options
+
+    this.$fetch = fetch
+    this.$uri = uri
+  }
+  $parsePayloadToFetchOptions (payload) {
+    return {
+      body: JSON.stringify({
+        jsonrpc: '2.0',
+        ...payload
+      }),
+      method: 'post',
+      mode: 'cors',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    }
+  }
+  $sendRequest (payload) {
+    const { $fetch, $uri } = this
+    const { id } = payload
+    const fetchOptions = this.$parsePayloadToFetchOptions(payload)
+
+    return $fetch($uri, fetchOptions)
+      .then(res => this.$processFethResponse(id, res))
+  }
+  $processFethResponse (id, res) {
+    if (!res.ok) {
+      return Promise.reject(Error(`HTTP ${res.status}: ${res.statusText}`))
+    }
+
+    return Promise.resolve(res.json())
+      .then(response => {
+        if (response.id !== id) {
+          return Promise.reject(new Error(`Invalid response id: ${response.id}`))
+        }
+
+        if (response.error) {
+          return Promise.reject(new RPCError(response.error))
+        }
+
+        return response.result
+      })
+  }
+  send (...args) {
+    return this.$sendRequest(
+      this.$makePayload(...args)
+    )
   }
 }
 
